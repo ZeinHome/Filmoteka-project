c2d512fc7a8d0de68fb9e58a73512ea9b287eee9
Ilya Pyatnichuk <zein3288@gmail.com>
2023-06-26 01:28:00 +0300

    Cursor

--- src/js/helpers/cursor.js
+++ src/js/helpers/cursor.js
@@ -1,32 +1,32 @@
-const el = document.querySelector('.follow-cursor');
+// const el = document.querySelector('.follow-cursor');
 
-const followCursor = () => {
-  window.addEventListener('mousemove', e => {
-    const target = e.target;
-    if (!target) return;
+// const followCursor = () => {
+//   window.addEventListener('mousemove', e => {
+//     const target = e.target;
+//     if (!target) return;
 
-    if (
-      target.closest('a') ||
-      target.closest('img') ||
-      target.closest('li') ||
-      target.closest('button') ||
-      target.closest('label') ||
-      target.closest('input')
-    ) {
-      el.classList.add('follow-cursor_active');
-    } else {
-      el.classList.remove('follow-cursor_active');
-    }
+//     if (
+//       target.closest('a') ||
+//       target.closest('img') ||
+//       target.closest('li') ||
+//       target.closest('button') ||
+//       target.closest('label') ||
+//       target.closest('input')
+//     ) {
+//       el.classList.add('follow-cursor_active');
+//     } else {
+//       el.classList.remove('follow-cursor_active');
+//     }
 
-    el.style.left = e.pageX + 'px';
-    el.style.top = e.pageY + 'px';
+//     el.style.left = e.pageX + 'px';
+//     el.style.top = e.pageY + 'px';
 
-    el.style.display = 'block';
-  });
-};
+//     el.style.display = 'block';
+//   });
+// };
 
-window.addEventListener('mouseout', () => {
-  el.style.display = 'none';
-});
+// window.addEventListener('mouseout', () => {
+//   el.style.display = 'none';
+// });
 
-followCursor();
+// followCursor();
